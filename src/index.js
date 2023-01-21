2250a73539ee21eb532f48b26cc2ab8bcd100237
KainAra <kainara.idwsi@gmail.com>
2023-01-21 08:39:40 +0300

    ref: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let stack = [];
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++)     {
+      if (str[i] == bracketsConfig[j][0] && bracketsConfig[j][0] !== bracketsConfig[j][1]) {
+        stack.push(str[i]);
+        break;
+      }
+      else if (str[i] == bracketsConfig[j][0] && bracketsConfig[j][0] == bracketsConfig[j][1]) {
+        if (count == 0) {
+          stack.push(str[i]);
+          count++;
+          break;
+        } else if (count > 0 && str[i] !== stack[stack.length -1]) {
+          stack.push(str[i]);
+          count++;
+          break;
+        } else {
+          let lastOpenBracket = stack.pop();
+          count--;
+          if (lastOpenBracket !== bracketsConfig[j][0]) {
+            return false;
+          }
+          break;
+        }
+      }
+      else if (str[i] == bracketsConfig[j][1] && bracketsConfig[j][0] !== bracketsConfig[j][1]) {
+        let lastOpenBracket = stack.pop();
+        if (lastOpenBracket !== bracketsConfig[j][0]) {
+          return false;
+        }
+        break;
+      }
+    }
+  }
+  return stack.length == 0 && count == 0;
 }
